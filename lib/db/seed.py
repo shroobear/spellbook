82492d04a30e8513e046670ce54375df664b5236
Stanley Shrewsbery <homestanrunner@gmail.com>
2023-08-11 21:13:46 -0400

    implements populate_spells function which seeds database with spell data

--- lib/db/seed.py
+++ lib/db/seed.py
@@ -13,12 +13,41 @@ response = requests.get("https://www.dnd5eapi.co/api/spells/")
 json_data = response.json()
 spells = json_data['results']
 
+def clear_table(table_class):
+    session.query(table_class).delete()
+    session.commit()
+
 def populate_spells():
+    clear_table(Spell)
+
     for entry in spells:
         r = requests.get(f"http://www.dnd5eapi.co{entry['url']}")
         spell_data = r.json()
-        print(spell_data)
-        import ipdb; ipdb.set_trace()
+
+        material = spell_data.get('material', None)
+        # damage_type = spell_data['damage']['damage_type']['name']
+        school = spell_data['school']['name']
+
+        # import ipdb; ipdb.set_trace()
+        new_spell = Spell(
+            name = spell_data['name'],
+            description = '/n'.join(spell_data['desc']),
+            casting_level = int(spell_data['level']),
+            components = ', '.join(spell_data['components']),
+            range = spell_data['range'],
+            material = material,
+            ritual = int(spell_data['ritual']),
+            duration = spell_data['duration'],
+            concentration = int(spell_data['concentration']),
+            casting_time = spell_data['casting_time'],
+            # damage_type = damage_type,
+            school = school
+        )
+        session.add(new_spell)
+
+    session.commit()
 
 
 populate_spells()
+
+import ipdb; ipdb.set_trace()
