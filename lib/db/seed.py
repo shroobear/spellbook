9e9b2401afcce8a5a22e5cc927e1df6fb25e2817
Stanley Shrewsbery <homestanrunner@gmail.com>
2023-08-12 07:33:24 -0400

    implements populate_user method which seeds user table with fake names

--- lib/db/seed.py
+++ lib/db/seed.py
@@ -1,10 +1,13 @@
 from models import Spell, User, Character
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
+from faker import Faker
 
 engine = create_engine("sqlite:///spell.db")
 Session = sessionmaker(bind=engine)
 session = Session()
+
+fake = Faker()
 
 import requests
 import json
@@ -33,6 +36,7 @@ def populate_spells():
             name = spell_data['name'],
             description = '/n'.join(spell_data['desc']),
             casting_level = int(spell_data['level']),
+            higher_level = spell_data['higher_level'],
             components = ', '.join(spell_data['components']),
             range = spell_data['range'],
             material = material,
@@ -48,6 +52,23 @@ def populate_spells():
     session.commit()
 
 
-populate_spells()
+def populate_user():
+    clear_table(User)
+
+    Faker.seed(0)
+    for _ in range(10):
+        user = fake.simple_profile()
+        new_user = User(
+            username = user['username'],
+            first_name = fake.first_name(),
+            last_name = fake.last_name(),
+        )
+        session.add(new_user)
+    session.commit()
+
+    
+
+# populate_spells()
+populate_user()
 
 import ipdb; ipdb.set_trace()
